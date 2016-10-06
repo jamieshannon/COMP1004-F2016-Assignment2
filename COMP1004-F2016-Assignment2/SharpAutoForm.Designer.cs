be84bb031f3e47ad43f275e37396e8cf1e9ba749
Christopher Shannon <200328763@student.georgianc.on.ca>
2016-10-06 14:21:31 -0400

    Added text boxes, labels and menu items

---
+++ COMP1004-F2016-Assignment2/SharpAutoForm.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace COMP1004_F2016_Assignment2
+{
+    partial class SharpAutoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BasePriceTextBox = new System.Windows.Forms.TextBox();
+            this.TradeInAllowanceTextBox = new System.Windows.Forms.TextBox();
+            this.BasePriceLabel = new System.Windows.Forms.Label();
+            this.TradeInAllowanceLabel = new System.Windows.Forms.Label();
+            this.AdditionalOptionsTextBox = new System.Windows.Forms.TextBox();
+            this.SubTotalTextBox = new System.Windows.Forms.TextBox();
+            this.SalesTaxTextBox = new System.Windows.Forms.TextBox();
+            this.TotalTextBox = new System.Windows.Forms.TextBox();
+            this.AmountDueTextBox = new System.Windows.Forms.TextBox();
+            this.AdditionalOptionsLabel = new System.Windows.Forms.Label();
+            this.SubTotalLabel = new System.Windows.Forms.Label();
+            this.SalesTaxLabel = new System.Windows.Forms.Label();
+            this.TotalLabel = new System.Windows.Forms.Label();
+            this.AmountDueLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EditToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CalculateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ClearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ColourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BasePriceTextBox
+            // 
+            this.BasePriceTextBox.Location = new System.Drawing.Point(142, 41);
+            this.BasePriceTextBox.Name = "BasePriceTextBox";
+            this.BasePriceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.BasePriceTextBox.TabIndex = 0;
+            // 
+            // TradeInAllowanceTextBox
+            // 
+            this.TradeInAllowanceTextBox.Location = new System.Drawing.Point(142, 76);
+            this.TradeInAllowanceTextBox.Name = "TradeInAllowanceTextBox";
+            this.TradeInAllowanceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.TradeInAllowanceTextBox.TabIndex = 1;
+            this.TradeInAllowanceTextBox.Text = "0";
+            // 
+            // BasePriceLabel
+            // 
+            this.BasePriceLabel.AutoSize = true;
+            this.BasePriceLabel.Location = new System.Drawing.Point(38, 48);
+            this.BasePriceLabel.Name = "BasePriceLabel";
+            this.BasePriceLabel.Size = new System.Drawing.Size(58, 13);
+            this.BasePriceLabel.TabIndex = 2;
+            this.BasePriceLabel.Text = "Base Price";
+            // 
+            // TradeInAllowanceLabel
+            // 
+            this.TradeInAllowanceLabel.AutoSize = true;
+            this.TradeInAllowanceLabel.Location = new System.Drawing.Point(38, 83);
+            this.TradeInAllowanceLabel.Name = "TradeInAllowanceLabel";
+            this.TradeInAllowanceLabel.Size = new System.Drawing.Size(98, 13);
+            this.TradeInAllowanceLabel.TabIndex = 3;
+            this.TradeInAllowanceLabel.Text = "Trade-in Allowance";
+            // 
+            // AdditionalOptionsTextBox
+            // 
+            this.AdditionalOptionsTextBox.Location = new System.Drawing.Point(142, 116);
+            this.AdditionalOptionsTextBox.Name = "AdditionalOptionsTextBox";
+            this.AdditionalOptionsTextBox.ReadOnly = true;
+            this.AdditionalOptionsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AdditionalOptionsTextBox.TabIndex = 4;
+            // 
+            // SubTotalTextBox
+            // 
+            this.SubTotalTextBox.Location = new System.Drawing.Point(142, 143);
+            this.SubTotalTextBox.Name = "SubTotalTextBox";
+            this.SubTotalTextBox.ReadOnly = true;
+            this.SubTotalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SubTotalTextBox.TabIndex = 5;
+            // 
+            // SalesTaxTextBox
+            // 
+            this.SalesTaxTextBox.Location = new System.Drawing.Point(142, 170);
+            this.SalesTaxTextBox.Name = "SalesTaxTextBox";
+            this.SalesTaxTextBox.ReadOnly = true;
+            this.SalesTaxTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SalesTaxTextBox.TabIndex = 6;
+            // 
+            // TotalTextBox
+            // 
+            this.TotalTextBox.Location = new System.Drawing.Point(142, 197);
+            this.TotalTextBox.Name = "TotalTextBox";
+            this.TotalTextBox.ReadOnly = true;
+            this.TotalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.TotalTextBox.TabIndex = 7;
+            // 
+            // AmountDueTextBox
+            // 
+            this.AmountDueTextBox.Location = new System.Drawing.Point(142, 224);
+            this.AmountDueTextBox.Name = "AmountDueTextBox";
+            this.AmountDueTextBox.ReadOnly = true;
+            this.AmountDueTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AmountDueTextBox.TabIndex = 8;
+            // 
+            // AdditionalOptionsLabel
+            // 
+            this.AdditionalOptionsLabel.AutoSize = true;
+            this.AdditionalOptionsLabel.Location = new System.Drawing.Point(41, 122);
+            this.AdditionalOptionsLabel.Name = "AdditionalOptionsLabel";
+            this.AdditionalOptionsLabel.Size = new System.Drawing.Size(92, 13);
+            this.AdditionalOptionsLabel.TabIndex = 9;
+            this.AdditionalOptionsLabel.Text = "Additional Options";
+            // 
+            // SubTotalLabel
+            // 
+            this.SubTotalLabel.AutoSize = true;
+            this.SubTotalLabel.Location = new System.Drawing.Point(41, 149);
+            this.SubTotalLabel.Name = "SubTotalLabel";
+            this.SubTotalLabel.Size = new System.Drawing.Size(53, 13);
+            this.SubTotalLabel.TabIndex = 10;
+            this.SubTotalLabel.Text = "Sub Total";
+            // 
+            // SalesTaxLabel
+            // 
+            this.SalesTaxLabel.AutoSize = true;
+            this.SalesTaxLabel.Location = new System.Drawing.Point(44, 176);
+            this.SalesTaxLabel.Name = "SalesTaxLabel";
+            this.SalesTaxLabel.Size = new System.Drawing.Size(54, 13);
+            this.SalesTaxLabel.TabIndex = 11;
+            this.SalesTaxLabel.Text = "Sales Tax";
+            // 
+            // TotalLabel
+            // 
+            this.TotalLabel.AutoSize = true;
+            this.TotalLabel.Location = new System.Drawing.Point(44, 203);
+            this.TotalLabel.Name = "TotalLabel";
+            this.TotalLabel.Size = new System.Drawing.Size(31, 13);
+            this.TotalLabel.TabIndex = 12;
+            this.TotalLabel.Text = "Total";
+            // 
+            // AmountDueLabel
+            // 
+            this.AmountDueLabel.AutoSize = true;
+            this.AmountDueLabel.Location = new System.Drawing.Point(47, 230);
+            this.AmountDueLabel.Name = "AmountDueLabel";
+            this.AmountDueLabel.Size = new System.Drawing.Size(66, 13);
+            this.AmountDueLabel.TabIndex = 13;
+            this.AmountDueLabel.Text = "Amount Due";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem,
+            this.EditToolStripMenuItem,
+            this.HelpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitToolStripMenuItem});
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F)));
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileToolStripMenuItem.Text = "File";
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.ExitToolStripMenuItem.Text = "Exit";
+            // 
+            // EditToolStripMenuItem
+            // 
+            this.EditToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CalculateToolStripMenuItem,
+            this.ClearToolStripMenuItem,
+            this.FontToolStripMenuItem,
+            this.ColourToolStripMenuItem});
+            this.EditToolStripMenuItem.Name = "EditToolStripMenuItem";
+            this.EditToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.E)));
+            this.EditToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.EditToolStripMenuItem.Text = "Edit";
+            // 
+            // CalculateToolStripMenuItem
+            // 
+            this.CalculateToolStripMenuItem.Name = "CalculateToolStripMenuItem";
+            this.CalculateToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.CalculateToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.CalculateToolStripMenuItem.Text = "Calculate";
+            // 
+            // ClearToolStripMenuItem
+            // 
+            this.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem";
+            this.ClearToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.ClearToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.ClearToolStripMenuItem.Text = "Clear";
+            // 
+            // FontToolStripMenuItem
+            // 
+            this.FontToolStripMenuItem.Name = "FontToolStripMenuItem";
+            this.FontToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.FontToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.FontToolStripMenuItem.Text = "Font";
+            // 
+            // ColourToolStripMenuItem
+            // 
+            this.ColourToolStripMenuItem.Name = "ColourToolStripMenuItem";
+            this.ColourToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.ColourToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.ColourToolStripMenuItem.Text = "Colour";
+            // 
+            // HelpToolStripMenuItem
+            // 
+            this.HelpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutToolStripMenuItem});
+            this.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem";
+            this.HelpToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.H)));
+            this.HelpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.HelpToolStripMenuItem.Text = "Help";
+            // 
+            // AboutToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            this.AboutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.AboutToolStripMenuItem.Text = "About";
+            // 
+            // SharpAutoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 322);
+            this.Controls.Add(this.AmountDueLabel);
+            this.Controls.Add(this.TotalLabel);
+            this.Controls.Add(this.SalesTaxLabel);
+            this.Controls.Add(this.SubTotalLabel);
+            this.Controls.Add(this.AdditionalOptionsLabel);
+            this.Controls.Add(this.AmountDueTextBox);
+            this.Controls.Add(this.TotalTextBox);
+            this.Controls.Add(this.SalesTaxTextBox);
+            this.Controls.Add(this.SubTotalTextBox);
+            this.Controls.Add(this.AdditionalOptionsTextBox);
+            this.Controls.Add(this.TradeInAllowanceLabel);
+            this.Controls.Add(this.BasePriceLabel);
+            this.Controls.Add(this.TradeInAllowanceTextBox);
+            this.Controls.Add(this.BasePriceTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SharpAutoForm";
+            this.Text = "Sharp Auto";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox BasePriceTextBox;
+        private System.Windows.Forms.TextBox TradeInAllowanceTextBox;
+        private System.Windows.Forms.Label BasePriceLabel;
+        private System.Windows.Forms.Label TradeInAllowanceLabel;
+        private System.Windows.Forms.TextBox AdditionalOptionsTextBox;
+        private System.Windows.Forms.TextBox SubTotalTextBox;
+        private System.Windows.Forms.TextBox SalesTaxTextBox;
+        private System.Windows.Forms.TextBox TotalTextBox;
+        private System.Windows.Forms.TextBox AmountDueTextBox;
+        private System.Windows.Forms.Label AdditionalOptionsLabel;
+        private System.Windows.Forms.Label SubTotalLabel;
+        private System.Windows.Forms.Label SalesTaxLabel;
+        private System.Windows.Forms.Label TotalLabel;
+        private System.Windows.Forms.Label AmountDueLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem EditToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CalculateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ClearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ColourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
+    }
+}
+
